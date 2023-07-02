1e80245695df078b1b7da59f3a4980079c02e701
roger-castaldo <roger.castaldo@gmail.com>
2023-07-02 10:55:32 -0400

    added RPC sample
    added in sample application for handling RPC commands

---
+++ Samples/Messages/Announcement.cs
@@ -0,0 +1,15 @@
+﻿using KubeMQ.Contract.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messages
+{
+    [MessageChannel("Announcements")]
+    public class Announcement
+    {
+        string Content { get; set; } = string.Empty;
+    }
+}
